4de37831e7db81f3a34dcb94e612109d4c06eeb0
AR <xxarxx0@gmail.com>
2023-10-18 12:11:01 +0300

    -mMove setting default values to add_argument options

--- main.py
+++ main.py
@@ -13,21 +13,22 @@ def main():
     #database_parser = subparsers.add_parser('db', help='database related commands')
     # config
     config_parser = subparsers.add_parser('config', help='config related commands')
-    config_parser.add_argument('-t', '--token', help='telegram bot token')
+    config_parser.add_argument('-t', '--token', required=True, help='telegram bot token')
     # server
     server_parser = subparsers.add_parser('server', help='server related commands')
     server_parser.add_argument('-d', '--debug', action='store_true', help='start in debug mode')
-    server_parser.add_argument('-p', '--port', help='listen port')
-    server_parser.add_argument('-ip', '--address', dest='ip', help='bind ip')
+    server_parser.add_argument('-p', '--port', default=5000, help='listen port')
+    server_parser.add_argument('-ip', '--address', dest='ip', default='127.0.0.1', help='bind ip')
 
 
     args = vars(parser.parse_args())
     if args['subcommand'] == 'config':
         make_config(args, 'config.xml')
+
     elif args['subcommand'] == 'server':
         debug = args['debug']
-        ip = args['ip'] or '127.0.0.1'
-        port = int(args['port']) if args['port'] else 5000
+        ip = args['ip'] 
+        port = int(args['port'])
 
         if debug:
             server.app.run(debug=True, host=ip, port=port)
