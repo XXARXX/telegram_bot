ecbeb86654319866b67ceb2dbaaad10566adc89e
AR <xxarxx0@gmail.com>
2023-10-24 16:18:26 +0300

    Rewrite _request function

--- src/telegram_api.py
+++ src/telegram_api.py
@@ -6,49 +6,153 @@ from .config import load_config
 HOST = 'api.telegram.org'
 REQ_TEMPLATE = '/bot{token}/{api_method}{query}'
 
-# TODO: Consider custom exceptions
-# TODO: Remove debug level
 class TelegramApi:
-    def __init__(self):
-        self.bot_token = load_config()['token']
+    """
+    Simple telegram REST api wrapper
+
+    Attributes
+    ----------
+    bot_token : str
+        Token used in telegram api
+    conn : HTTPConnection
+        Telegram api http connection
+
+    Methods
+    -------
+
+    get_me()
+        Returns information about bot
+    send_message(chat_id, text)
+        Sends text to chat_id chat
+    get_updates()
+        Return updates related with bot
+    _request(request_method, api_method, api_data='')
+        Utility method for communicating with telegram bot api
+    """
+    def __init__(self, bot_token = None):
+        self.bot_token = bot_token
         self.conn = client.HTTPSConnection(HOST)
 
     def get_me(self):
-        data = self._request('GET', 'getMe')
-        return data
+        """
+        Returns
+        -------
+        dictionary
+            status
+                request status
+            error_message optional
+                if status 'error' contain error description
+            data optional
+                if status 'success' contain returned data
+        """
+        return self._request('GET', 'getMe')
 
     def send_message(self, chat_id, text):
+        """
+        Returns
+        -------
+        dictionary
+            status
+                request status
+            error_message optional
+                if status 'error' contain error description
+            data optional
+                if status 'success' contain returned data
+        """
         api_data = {
             'chat_id': chat_id,
             'text': text
         }
-        data = self._request('POST', 'sendMessage', api_data)
-        return data
+        return self._request('POST', 'sendMessage', api_data)
     
     def get_updates(self):
-        data = self._request('GET', 'getUpdates')
+        """
+        Returns
+        -------
+        dictionary
+            status
+                request status
+            error_message optional
+                if status 'error' contain error description
+            data optional
+                if status 'success' contain returned data
+        """
+        return self._request('GET', 'getUpdates')
 
     # TODO: get query
     def _request(self, request_method, api_method, api_data = ''):
+        """
+        Parameters
+        ----------
+        request_method : str
+            HTTP method GET or POST
+        api_method : str
+            api endpoint
+        api_data : str
+            data for endpoint
+
+        Returns
+        -------
+        dictionary
+            status
+                request status
+            error_message optional
+                if status 'error' contain error description
+            data optional
+                if status 'success' contain returned data
+        
+        Raises
+        ------
+        NotImplementedError
+            If trying sending api_data trough GET method
+        http.client.HTTPException
+            If trying use other methods except GET and POST
+        """
         url = ''
         query = ''
-        if request_method in ['GET', 'get']:
-            if query != '':
-                raise NotImplementedError('get query not supported')
-            url = REQ_TEMPLATE.format(token=self.bot_token, api_method=api_method, query=query)
-            self.conn.request(request_method, url)
-        elif request_method in ['POST', 'post']:
-            url = REQ_TEMPLATE.format(token=self.bot_token, api_method=api_method, query=query)
-            self.conn.request(request_method, url, body=json.dumps(api_data), headers={'Content-Type': 'application/json'})
-        else:
-            raise Exception('unsupported request method: %s' % request_method)
+        result = None
+        try:
+            if request_method in ['GET', 'get']:
+                if query != '':
+                    raise NotImplementedError('get query not supported')
+                url = REQ_TEMPLATE.format(token=self.bot_token, api_method=api_method, query=query)
+                self.conn.request(request_method, url)
+                    
+            elif request_method in ['POST', 'post']:
+                url = REQ_TEMPLATE.format(token=self.bot_token, api_method=api_method, query=query)
+                self.conn.request(request_method, url, body=json.dumps(api_data), headers={'Content-Type': 'application/json'})
+            else:
+                raise client.HTTPException('unsupported request method: %s' % request_method)
+        except client.HTTPException as e:
+            result = {
+                'status': 'error',
+                'error_message': e
+            }
+        except TimeoutError:
+            result = {
+                'status': 'error',
+                'error_message': 'cant connect to remote host'
+            }
 
-        response = self.conn.getresponse()
-        if response.status != 200:
-            print('request error {status}: {reason}'.format(status=response.status, reason=response.reason))
-        response.body = json.load(response)
-        if not response.body.get('ok'):
-            print('server return error {0}'.format(body))
+        if result is None:
+            response = self.conn.getresponse()
+            if response.status != 200:
+                result = {
+                    'status': 'error',
+                    'error_message': response.reason
+                }
+            response.body = json.load(response)
+            if not response.body.get('ok'):
+                result = {
+                    'status': 'error',
+                    'error_message': response.body.get('description')
+                }
+            else:
+                result = {
+                    'status': 'success',
+                    'data': response.body.get('result')
+                }
 
-        self.conn.close()
-        return vars(response)
+            self.conn.close()
+
+        return result
--- tests/test_telegram_api.py
+++ tests/test_telegram_api.py
@@ -1,16 +1,23 @@
 import unittest
 import os
 
+from src import config
 from src.telegram_api import TelegramApi
 
 class TestTelegramApi(unittest.TestCase):
     def setUp(self):
         self.api = TelegramApi()
+        conf_path = 'test_config.xml'
+        c = config.load_config(conf_path)
+        self.api.bot_token = c['token']
+        self.config = c
 
     def test_get_me(self):
         result = self.api.get_me()
-        self.assertEqual(result.get('status'), 200)
-        body = result.get('body')
-        self.assertEqual(body.get('ok'), True)
-        user = body.get('result')
+        self.assertEqual(result.get('status'), 'success')
+        user = result.get('data')
         self.assertEqual(user.get('is_bot'), True)
+
+    def test_send_message(self):
+        result = self.api.send_message(self.config['chatid'], 'test')
+        self.assertEqual('success', result.get('status'))
