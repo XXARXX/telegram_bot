bbfc08feb23129cf3ed55b237ecf5fe5288e4034
AR <xxarxx0@gmail.com>
2023-10-24 16:22:54 +0300

    Add test section to cli; Add ability to choose database file path

--- src/main.py
+++ src/main.py
@@ -12,12 +12,17 @@ def main():
     subparsers = parser.add_subparsers(required=True, dest='subcommand')
     # database
     database_parser = subparsers.add_parser('db', help='database related commands')
-    database_parser.add_argument('-c', '--create', dest='dbname', required=False, help='create new database')
-    database_parser.add_argument('-i', '--insert', dest='row', action='store_true', help='insert row')
+    database_parser.add_argument('-c', '--create', dest='dbname', help='create new database')
     database_parser.add_argument('-g', '--get-status', dest='id')
     # config
     config_parser = subparsers.add_parser('config', help='config related commands')
     config_parser.add_argument('-t', '--token', required=True, help='telegram bot token')
+    config_parser.add_argument('-d', '--database', default='default.db', help='database path')
+    # test config
+    test_parser = subparsers.add_parser('test', help='testing related commands')
+    test_parser.add_argument('-t', '--token', required=True, help='telegram bot token used for testing')
+    test_parser.add_argument('-d', '--database', default='test.db', help='name of database used for testing')
+    test_parser.add_argument('-c', '--chatid', required=True, help='chat for testing')
     # server
     server_parser = subparsers.add_parser('server', help='server related commands')
     server_parser.add_argument('-d', '--debug', action='store_true', help='start in debug mode')
@@ -36,21 +41,21 @@ def main():
         port = int(args['port'])
 
         if debug:
-            server.app.run(debug=True, host=ip, port=port)
+            server.create_app().run(debug=True, host=ip, port=port)
         else:
             print('run server on {0}:{1} in production mode'.format(ip, port))
-            serve(server.app, host=ip, port=port, threads=4)
+            serve(server.create_app(), host=ip, port=port, threads=4)
 
     elif subcommand == 'db':
         dbname = args['dbname']
-        row = args['row']
         msg_id = args['id']
         if dbname:
-            sql.create_db('db/{0}'.format(dbname))
-        elif row:
-            sql.insert({'id': 'test', 'status': 'test'})
+            sql.create_db('{0}'.format(dbname))
         elif msg_id:
             sql.get(msg_id)
+
+    elif subcommand == 'test':
+        make_config(args, 'test_config.xml')
 
 if __name__ == '__main__':
     main()
